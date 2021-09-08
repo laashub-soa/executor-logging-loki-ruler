ed7b8d704ea9ae65cda6930700b5084c248ed44d
tristan <tanshilinmail@gmail.com>
2021-09-08 14:14:06 +0800

    Update loki.py

--- component/loki.py
+++ component/loki.py
@@ -1,4 +1,6 @@
+import json
 import logging
+import time
 from urllib.parse import urlencode
 
 import requests
@@ -16,12 +18,15 @@ def query(base_url, log_ql, query_time_start, query_time_end):
     :param query_time_end:
     :return:
     """
-    logger.debug("base_url: %s\n, log_ql: %s\n, query_time_start: %s\n, query_time_end: %s\n" % (
+    logger.debug("params: base_url: %s\n, log_ql: %s\n, query_time_start: %s\n, query_time_end: %s\n" % (
         base_url, log_ql, query_time_start, query_time_end))
+    execute_time_start = time.time()
     query_result = requests.get(base_url + "/loki/api/v1/query_range",
                                 params=urlencode(
                                     {"query": log_ql, "start": query_time_start, "end": query_time_end}))
-    logger.debug("query_result: %s" % query_result.text)
+    query_result_text = query_result.text
+    logger.debug(
+        "query_time_cost: %s秒 \n,query_result: %s" % (int(time.time() - execute_time_start), query_result_text))
     if query_result.status_code != 200:
-        raise Exception("请求失败")
-    return query_result.json()
+        raise Exception(query_result_text)
+    return json.loads(query_result_text)
